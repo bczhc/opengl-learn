915e1554b219120305a944bb1a68a54bbb6d7aa3
bczhc <bczhc0@126.com>
2021-08-26 11:54:32 +0800

    learn_opengl: transformations-exercise1

--- learn-opengl/transformations/main.cpp
+++ learn-opengl/transformations/main.cpp
@@ -114,8 +114,15 @@ int main() {
         program->bind();
 
         glm::mat4 trans(1.0F);
+#ifdef CODE_MAIN
         trans = glm::translate(trans, translateVec);
         trans = glm::rotate(trans, (f32) glfwGetTime(), rotatePivot);
+#elif defined(CODE_EXERCISE1)
+        trans = glm::rotate(trans, (f32) glfwGetTime(), rotatePivot);
+        trans = glm::translate(trans, translateVec);
+        // the reason is because glm transformations do a pre-multiplication, so the
+        // transformation order is reversed
+#endif
         transformationMatrix.setUniformMatrix(glm::value_ptr(trans));
 
         glBindVertexArray(va);
